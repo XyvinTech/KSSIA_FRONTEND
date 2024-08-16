de184048516146a94bf1187d84b8becadbd38df3
anjanavu <anjanavu2000@gmail.com>
2024-08-16 17:56:51 +0530

    ui changes

--- src/pages/Admin/DashboardPage.jsx
+++ src/pages/Admin/DashboardPage.jsx
@@ -58,7 +58,7 @@ const DashboardPage = () => {
 
 
      <Grid>
-     <Grid container spacing={1} item xs={12}>
+     <Grid container spacing={1} item xs={12}padding={'10px'}>
       <Grid item xs={6} >
         <Box padding={2}>
         <DashboardRevnue/>
