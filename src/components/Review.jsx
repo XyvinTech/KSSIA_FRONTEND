1ef4f6ac1d1a2cad6a6179fd2b200c742a6f7cb0
anjanavu <anjanavu2000@gmail.com>
2024-11-12 12:55:48 +0530

    #review

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -1,30 +1,40 @@
 import React from "react";
-import { Grid, Typography } from "@mui/material";
-import reviews from "../assets/json/ReviewData";
+import { Grid, Typography, Box } from "@mui/material";
 import StyledReview from "../ui/StyledReview";
+import styled from "styled-components";
+
+const StyledNoReviewsMessage = styled(Typography)`
+  font-size: 1.2rem;
+  color: #a0a0a0;
+  font-weight: 500;
+  text-align: center;
+  margin-top: 16px;
+`;
+
+const ReviewContainer = styled(Grid)`
+  padding: 16px;
+  border-radius: 12px;
+  background-color: #f7f7f9;
+  box-shadow: 0px 4px 12px rgba(0, 0, 0, 0.1);
+`;
 
 const Review = ({ data }) => {
   return (
-    <Grid container item xs={12} padding={2} borderRadius={"12px"}>
-      {data && data?.length > 0 && (
-        <Typography
-          variant="h6"
-          color={"#2C2829"}
-          fontWeight={700}
-          sx={{ padding: "0 0 16px 0", fontSize: 16 }}
-        >
-          Member reviews
-        </Typography>
+    <ReviewContainer container item xs={12}>
+      {data?.length > 0 ? (
+        <Grid container spacing={5} pt={2}>
+          {data.map((review) => (
+            <Grid item md={6} xs={12} key={review?.id}>
+              <StyledReview review={review} />
+            </Grid>
+          ))}
+        </Grid>
+      ) : (
+        <Box width="100%" display="flex" justifyContent="center">
+          <StyledNoReviewsMessage>No reviews available</StyledNoReviewsMessage>
+        </Box>
       )}
-
-      <Grid container spacing={5} pt={2}>
-        {data?.map((review) => (
-          <Grid item md={6} xs={12} key={review?.id} spacing={2}>
-            <StyledReview review={review} />
-          </Grid>
-        ))}
-      </Grid>
-    </Grid>
+    </ReviewContainer>
   );
 };
 
--- src/pages/Admin/Members/MembersSinglepage.jsx
+++ src/pages/Admin/Members/MembersSinglepage.jsx
@@ -20,7 +20,22 @@ import axiosInstance from "../../../api/axios-interceptor";
 import CONSTANTS from "../../../constants";
 import { useParams } from "react-router-dom";
 import { usePaymentStore } from "../../../store/payment-store";
+import styled from "styled-components";
+const StyledNoReviewsMessage = styled(Typography)`
+  font-size: 1.2rem;
+  color: #a0a0a0;
+  font-weight: 500;
+  text-align: center;
+  margin-top: 16px;
+`;
 
+const ReviewContainer = styled(Grid)`
+  padding: 16px;
+  border-radius: 12px;
+  width: 100%;
+  background-color: #f7f7f9;
+  box-shadow: 0px 4px 12px rgba(0, 0, 0, 0.1);
+`;
 const MembersSinglepage = () => {
   const [selectedTab, setSelectedTab] = useState(0);
   const [userData, setUserData] = useState({});
@@ -100,7 +115,7 @@ const MembersSinglepage = () => {
         <Tab label="Subscriptions" />
         <Tab label="Products" />
         <Tab label="Requirements" />
-        <Tab label="Analytics" />
+        <Tab label="Reviews" />
       </Tabs>
       <Box padding="15px" marginBottom={4}>
         {selectedTab === 0 && (
@@ -114,10 +129,10 @@ const MembersSinglepage = () => {
           </Grid>
         )}
         {selectedTab === 2 && (
-          <Grid container>
-            <Stack direction={"column"} spacing={3}>
+          <Grid container >
+           
               {cards && cards?.length > 0 ? (
-                <>
+               <Stack direction={"column"} spacing={3}  >
                   {cards.some((p) => p.category === "app") && (
                     <AppSubscriptionCard
                       payment={cards.find((p) => p.category === "app")}
@@ -129,13 +144,16 @@ const MembersSinglepage = () => {
                       onChange={handleIsChange}
                     />
                   )}
-                </>
+                </Stack>
               ) : (
-                <Typography variant="h6" textAlign="center">
-                  No Subscription
-                </Typography>
+                <ReviewContainer>
+                  <Box width="100%" display="flex" justifyContent="center">
+                    <StyledNoReviewsMessage>
+                      No Subscription
+                    </StyledNoReviewsMessage>
+                  </Box>
+                </ReviewContainer>
               )}
-            </Stack>
           </Grid>
         )}
 
@@ -151,10 +169,10 @@ const MembersSinglepage = () => {
         )}
         {selectedTab === 5 && (
           <Grid container item xs={12}>
-            <Grid item xs={12}>
+            {/* <Grid item xs={12}>
               <MemberAnalytics />
-            </Grid>
-            <Grid item xs={12} marginTop={4}>
+            </Grid> */}
+            <Grid item xs={12} padding={2}>
               <Review data={userData?.reviews} />
             </Grid>
           </Grid>
