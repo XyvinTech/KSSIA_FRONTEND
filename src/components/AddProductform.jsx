da50008d87c9960e585a3796192126e5623ab8e7
anjanavu <anjanavu2000@gmail.com>
2024-11-15 14:34:51 +0530

    #tagsList

--- src/components/AddProductform.jsx
+++ src/components/AddProductform.jsx
@@ -357,7 +357,7 @@ export default function Addproductform() {
                     </Typography>
                   </Stack>
                   <Box mt={2}>
-                    {tags.map((tag, index) => (
+                    {tags?.map((tag, index) => (
                       <Stack
                         direction="row"
                         key={index}
